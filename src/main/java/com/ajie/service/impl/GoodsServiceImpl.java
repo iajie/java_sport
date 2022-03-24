3b3208bc40e7f5837628c37a9878809810462154
ajie <ajie20999@163.com>
2022-03-24 19:25:13 +0800

    删除bug

--- src/main/java/com/ajie/service/impl/GoodsServiceImpl.java
+++ src/main/java/com/ajie/service/impl/GoodsServiceImpl.java
@@ -24,9 +24,6 @@ public class GoodsServiceImpl implements GoodsService {
     @Autowired
     private GoodsMapper goodsMapper;
 
-    @Autowired
-    private SysUserService userMapper;
-
     @Override
     public Result findPage(QueryInfo queryInfo) {
         PageHelper.startPage(queryInfo.getPageNumber(), queryInfo.getPageSize());
